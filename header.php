1b27064bedbe8de60cd449a4e3da10f27a15fe4e
hanifafissalma <halimiliul@gmail.com>
2019-12-31 14:17:29 +0700

    fix header and add image

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <header class="masthead">
-  <nav class="navbar navbar-expand-md navbar-light rounded mb-3" style="background-color: #e3f2fc;">
+  <nav class="navbar navbar-expand-md navbar-light rounded mb-3">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
